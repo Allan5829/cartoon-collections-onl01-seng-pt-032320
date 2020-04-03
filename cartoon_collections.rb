9403f3fa6208c60fb1a760979b104ef725facab3
Allan Farca <allan.farca@gmail.com>
2020-04-03 05:34:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,18 @@ def summon_captain_planet(veggies)
 end
 
 def long_planeteer_calls (calls_long)
-  # Your code here
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
 def find_the_cheese# code an argument here
