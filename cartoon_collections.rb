a602e53a042c473c7437057c9dd0676825c1606f
Allan Farca <allan.farca@gmail.com>
2020-04-03 05:41:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,6 @@ def long_planeteer_calls (calls_long)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese (cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
