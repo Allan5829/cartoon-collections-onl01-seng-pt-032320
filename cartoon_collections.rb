7593b1ac3f4106924ffe59855373a2a0de1614d0
Allan Farca <allan.farca@gmail.com>
2020-04-03 05:27:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(veggies)
   veggies.map! { |veggy| veggy.capitalize }
-  
+  veggies.collect {|veggy| veggy + "!"}
 end
 
 def long_planeteer_calls# code an argument here
