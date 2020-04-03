61c6d91f16fd1616ea10b48fa3139dfaeb63fb44
Allan Farca <allan.farca@gmail.com>
2020-04-03 05:38:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,10 +21,10 @@ def long_planeteer_calls (calls_long)
     i = i + 1
   end
  
-  if block_return_values.include?(false)
-    false
-  else
+  if block_return_values.include?(true)
     true
+  else
+    false
   end
 end
 
